bf7a8f3a96b4ad609a886984c0d4f87d57a15024
janderton <32845311+janderton@users.noreply.github.com>
2018-12-11 22:54:39 -0600

    updating hello

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "James' golang http example"
+	title := "James' Updated golang http example"
 
 	from := ""
 	if r.URL != nil {
